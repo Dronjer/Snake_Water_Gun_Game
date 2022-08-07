81d0bd95c5a432d2a1408698c937d9e07349bb47
Dronjer <110643330+Dronjer@users.noreply.github.com>
2022-08-07 13:12:45 -0400

    Add files via upload

---
+++ Sname Water adn Gun Game Using For loop.py
@@ -0,0 +1,53 @@
+import random
+print("This is Snake (S), Water(W) and  Gun(G) Game")
+Player2 = ["S","W","G"]
+tabulate_wins = 0
+tabulate_ties = 0
+tabulate_losses = 0
+for i in range(1,11):
+    print("Trial number by the user", i)
+    P1 = input()
+    P2 = random.choice(Player2)
+    print(P2)
+    if P1 == "S":
+        if P2 == P1:
+            print("Match is tie")
+            tabulate_ties += 1
+        elif P2 == "G":
+            print("P1 losses the match")
+            tabulate_losses += 1
+        else:
+            print("P1 wins the match")
+            tabulate_wins += 1
+    elif P1 == "G":
+        if P2 == P1:
+            print("Match is tie")
+            tabulate_ties += 1
+        elif P2 == "W":
+            print("P1 losses the match")
+            tabulate_losses += 1
+        else:
+            print("P1 wins the match")
+            tabulate_wins += 1
+    elif P1 == "W":
+        if P2 == P1:
+            print("Match is tie")
+            tabulate_ties += 1
+        elif P2 == "S":
+            print("P1 losses the match")
+            tabulate_losses += 1
+        else:
+            print("P1 wins the match")
+            tabulate_wins += 1
+
+
+
+print(tabulate_losses)
+print(tabulate_wins)
+print(tabulate_ties)
+
+
+
+
+
+
